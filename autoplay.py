998fbcdee10d07cf72d57ff842d690a81978fbc3
qiaork-ipad <1229319295@qq.com>
2021-04-06 16:46:36 +0000

    refactor autoplay (set_dir dup more_merged) & feat count

--- autoplay.py
+++ autoplay.py
@@ -3,10 +3,12 @@ import random
 import logic
 import constants as c
 
+import csv
+
 
 class GameGrid():
 
-    def __init__(self, strtg=2.2):
+    def __init__(self, strtg="1.4"):
 
         # prepare game 2048
 
@@ -17,75 +19,51 @@ class GameGrid():
         self.history_matrixs = []
         self.history_matrixs.append(self.matrix)
         self.history_matrixs.append(self.matrix)
+        self.score = 0
         print(self.matrix)
         
         # prepare game strategy
-        dirs = [c.KEY_UP, c.KEY_LEFT, c.KEY_RIGHT, c.KEY_DOWN]
+        strtg = strtg.split(".")
+        for n, s in enumerate(strtg):
+            strtg[n] = int(s)
+
+        self.dir = []
+        self.dirs = [c.KEY_UP, c.KEY_LEFT, c.KEY_RIGHT, c.KEY_DOWN]
+        self.history_dir = -1
+        
         # for strategy1 direction-first
-        if int(strtg) == 1:
-            if strtg == 1.1:  # line
-                dir = [c.KEY_UP]
-            elif strtg == 1.2:  # corner
-                dir = [c.KEY_UP, c.KEY_LEFT]
-            elif strtg == 1.4:  # serpentine(snakelike)
-                dir = [c.KEY_UP, c.KEY_LEFT]
-            for d in dir:
-                dirs.remove(d)
-            history_dir = -1
-        elif int(strtg == 2):
-            return
+        if strtg[0] == 1:
+            if strtg[1] == 1:  # line
+                self.set_dir([c.KEY_UP])
+            elif strtg[1] == 2:  # corner
+                self.set_dir([c.KEY_UP, c.KEY_LEFT])
+            elif strtg[1] == 4:  # serpentine(snakelike)
+                self.set_dir([c.KEY_UP, c.KEY_LEFT])
         
         # play
         while True:
-            if int(strtg) == 0:
-                d = dirs[random.randint(0,len(dir)-1)]
-            elif int(strtg) == 1:
+            if strtg[0] == 0:
+                d = self.dirs[random.randint(0,len(self.dirs)-1)]
+            elif strtg[0] == 1:
                 # for strategy1.4 serpentine(snakelike), when change direction
-                if strtg == 1.4 and self.matrix[0][0] == 256 and self.matrix[0][c.GRID_LEN - 1] != 0:
-                    dirs = [c.KEY_UP, c.KEY_RIGHT, c.KEY_LEFT, c.KEY_DOWN]
+                if strtg[1] == 4 and self.matrix[0][0] == 256 and self.matrix[0][c.GRID_LEN - 1] != 0:
+                    self.set_dir([c.KEY_UP, c.KEY_RIGHT])
                     print("dirs change 256")
                 # for strategy1, when invalid direction
-                if self.matrix == self.history_matrixs[-2]:  # duplicate
-                    history_dir = history_dir + 1
-                    print("    ", history_dir)
-                    if history_dir < len(dir):  # try wanted directions
-                        d = dir[history_dir]
-                    else:  # try other directions
-                        d = dirs[history_dir - len(dir)]
-                else:
-                    history_dir = -1
-                    d = dir[random.randint(0, len(dir)-1)]
-            elif int(strtg) == 2:
-                merged = []
-                if strtg == 2.1:  #strategy2.1 more merged_numbers
-                    for d in dirs:
-                        m, d, n, g = self.commands[d](self.matrix)
-                        merged.append(n)
-                    for m in merged:
-                        if m != 0:
-                            d = dirs[merged.index(max(merged))]
-                            break
-                    if d != dirs[merged.index(max(merged))]:
-                        d = dirs[random.randint(0,len(dirs)-1)]
-                    print(d, merged)
-                if strtg == 2.2:  #strategy2.2 more merged_grids
-                    for d in dirs:
-                        m, d, n, g = self.commands[d](self.matrix)
-                        merged.append(g)
-                    for m in merged:
-                        if m != 0:
-                            d = dirs[merged.index(max(merged))]
-                            break
-                    if d != dirs[merged.index(max(merged))]:
-                        d = dirs[random.randint(0,len(dirs)-1)]
-                    print(d, merged)
-            else:
-                return False
+                d = self.dup()
+                if d == False:
+                    self.history_dir = -1
+                    d = self.dir[random.randint(0, len(self.dir)-1)]
+            elif strtg[0] == 2:
+                d = self.more_merged(strtg[1])
+                if d == False:
+                    d = self.dirs[random.randint(0,len(self.dirs)-1)]
             # key down
             state = self.key_down(d)
             print(self.matrix, state)
             if state != 'not over':
                 break  # game over
+        self.record()
         print(strtg)
         return
 
@@ -93,9 +71,60 @@ class GameGrid():
         key = dir
         if key in self.commands:
             self.matrix, done, n, g = self.commands[dir](self.matrix)
+            self.score += n * 2
             if done:
                 self.matrix = logic.add_two(self.matrix)
             self.history_matrixs.append(self.matrix)
         return logic.game_state(self.matrix)
+        
+    def set_dir(self, dir):
+        self.dir = dir
+        for d in dir:
+            self.dirs.remove(d)
+    
+    def dup(self):
+        if self.matrix == self.history_matrixs[-2]:  # duplicate
+            self.history_dir = self.history_dir + 1
+            print("    ", self.history_dir)
+            if self.history_dir < len(self.dir):  # try wanted directions
+                d = self.dir[self.history_dir]
+            else:  # try other directions
+                d = self.dirs[self.history_dir - len(self.dir)]
+            return d
+        else:
+            return False
+            
+    def more_merged(self, n_g):
+        merged = []
+        for d in self.dirs:
+            m, d, n, g = self.commands[d](self.matrix)
+            l = [n,g]
+            merged.append(l[n_g-1])
+        print(merged)
+        for md in merged:
+            if md != 0:
+                d = self.dirs[merged.index(max(merged))]
+                return d
+        if d != self.dirs[merged.index(max(merged))]:
+            return False
+            
+    def max(self):
+        max = 2
+        for i in self.matrix:
+            for j in i:
+                if j > max:
+                    max = j
+        return max
+        
+    def record(self):
+        max = self.max()
+        print(max, self.score)
+
+
+if False:
+    f = open('total.csv','w',encoding='utf-8')  # count total numbers
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(["","s","s0","s1","s2","s1.1","s1.2","s1.4","s2.1","s2.2","s2.1.0","s2.1.1","s2.2.0","s2.2.1"])
+    f.close()
 
 game_grid = GameGrid()
